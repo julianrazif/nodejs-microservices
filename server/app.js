96e04a8dbd7a39a7113052dd889edffccbb197c1
JULIAN RAZIF FIGARO <julian.razif@gmail.com>
2020-11-21 21:10:56 +0700

    Fixing app.js: remove listen function, and move it to http.js

--- server/app.js
+++ server/app.js
@@ -4,6 +4,4 @@ const app = express()
 const PORT = process.env.PORT || 3000
 app.set('port', PORT)
 
-app.listen(app.get('port'), console.log(`server up and running in ${process.env.NODE_ENV} on PORT: ${app.get('port')}`))
-
 module.exports = app
