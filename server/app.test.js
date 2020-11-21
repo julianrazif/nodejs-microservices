67f43e82730eec616532328beff0861bd475a6c9
JULIAN RAZIF FIGARO <julian.razif@gmail.com>
2020-11-21 21:17:42 +0700

    Adding app test

---
+++ server/app.test.js
@@ -0,0 +1,19 @@
+const supertest = require('supertest')
+const app = require('./app')
+
+const agent = supertest(app)
+
+describe('Test App functionality', () => {
+  test('Should fail when request page', async (done) => {
+    try {
+      const res = await agent.get('/').expect('Content-Type', /json/)
+
+      expect(res.status).toBe(200)
+      expect(res.body).toHaveProperty('message', 'this is server 01')
+
+      done()
+    } catch (err) {
+      done(err)
+    }
+  })
+})
